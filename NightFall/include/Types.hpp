a50b1876fa94aa9ceea57e6d90a25faa8a8121c4
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-09-11 20:38:11 +0100

    Update Types.hpp

--- NightFall/include/Types.hpp
+++ NightFall/include/Types.hpp
@@ -24,5 +24,5 @@ SOFTWARE.*/
 #include <cstdbool>
 #include <cstdalign>
 
-//#define R_SUCCEEDED(res) (res == 0)
-//#define R_FAILED(res)    (res != 0)
+#define R_SUCCEEDED(res) (res == 0)
+#define R_FAILED(res)    (res != 0)
